a5fc4052435d27e65cd234ab8c2ee236216b518c
Sorin37 <glizard.glizard@yahoo.com>
2023-01-17 19:18:19 +0200

    Added additional fields for the db changes

--- NewsApi/Model/Announcement.cs
+++ NewsApi/Model/Announcement.cs
@@ -6,8 +6,10 @@
         public string Title { get; set; }
         public string Message { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
         public string Author { get; set; }
         public string ImageUrl { get; set; }
+        public string CategoryId { get; set; }
 
     }
 }
