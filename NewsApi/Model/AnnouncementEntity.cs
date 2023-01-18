b6fe034d8b93b0e900911925e25d2ae47de5cfc9
Sorin37 <glizard.glizard@yahoo.com>
2023-01-18 19:27:13 +0200

    Last changes

--- NewsApi/Model/AnnouncementEntity.cs
+++ NewsApi/Model/AnnouncementEntity.cs
@@ -7,6 +7,8 @@
         public string Category { get; set; }
         public string Author { get; set; }
         public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public string CategoryId { get; set; }
 
         public Announcement EntityToModel()
         {
@@ -15,7 +17,9 @@
                 Message = Message, 
                 Category = Category,
                 Author = Author,
-                ImageUrl = ImageUrl
+                ImageUrl = ImageUrl,
+                Description = Description,
+                CategoryId = CategoryId
             };
         }
     }
